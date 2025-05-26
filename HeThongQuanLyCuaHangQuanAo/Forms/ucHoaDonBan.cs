e7dea8985ec7ba7bcade4cf648f9e3c8c52a259d
NguyenHuuAn2005 <anotrynotpass1@gmail.com>
2025-05-26 12:01:41 +0700

    Update ucHoaDonBan.cs

--- HeThongQuanLyCuaHangQuanAo/Forms/ucHoaDonBan.cs
+++ HeThongQuanLyCuaHangQuanAo/Forms/ucHoaDonBan.cs
@@ -1,13 +1,17 @@
-﻿using System;
+using HeThongQuanLyCuaHangQuanAo.BUS;
+using HeThongQuanLyCuaHangQuanAo.DAL;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using HeThongQuanLyCuaHangQuanAo.BUS;
 
 namespace HeThongQuanLyCuaHangQuanAo.Forms
 {
@@ -88,5 +92,82 @@ namespace HeThongQuanLyCuaHangQuanAo.Forms
                 LoadData();
             }
         }
+                private void btnXuatHoaDonBan_Click(object sender, EventArgs e)
+        {
+            if (materialListView1.SelectedItems.Count > 0)
+            {
+                string soHDB = materialListView1.SelectedItems[0].SubItems[0].Text;
+                XuatHoaDonBanRaPDF(soHDB);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn hóa đơn để xuất.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void XuatHoaDonBanRaPDF(string soHDB)
+        {
+            var chiTietDAL = new ChiTietHDBanDAL();
+            var chiTietList = chiTietDAL.GetChiTietByHDBanID(soHDB);
+
+            if (chiTietList == null || chiTietList.Count == 0)
+            {
+                MessageBox.Show("Không tìm thấy chi tiết hóa đơn để xuất.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var hd = chiTietList.First();
+
+            string folderPath = @"C:\HoaDonBan";
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            string fileName = $"HoaDonBan_{hd.SoHDB}.pdf";
+            string filePath = Path.Combine(folderPath, fileName);
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Create))
+            using (Document doc = new Document(PageSize.A4))
+            using (PdfWriter writer = PdfWriter.GetInstance(doc, fs))
+            {
+                doc.Open();
+                var font = FontFactory.GetFont(BaseFont.TIMES_ROMAN, 12, BaseColor.BLACK);
+
+                doc.Add(new Paragraph("HOA DON BAN", FontFactory.GetFont(BaseFont.TIMES_BOLD, 16, BaseColor.BLUE)));
+                doc.Add(new Paragraph($"So HD: {hd.SoHDB}"));
+                doc.Add(new Paragraph($"Ngay ban: {hd.NgayBan:dd/MM/yyyy}"));
+                doc.Add(new Paragraph($"Nhan vien: {hd.TenNV}"));
+                doc.Add(new Paragraph($"Khach hang: {hd.TenKhach}"));
+                doc.Add(new Paragraph(" "));
+
+                PdfPTable table = new PdfPTable(5);
+                table.WidthPercentage = 100;
+                table.AddCell("Ten san pham");
+                table.AddCell("Don gia");
+                table.AddCell("So luong");
+                table.AddCell("Giam gia");
+                table.AddCell("Thanh tien");
+
+                decimal tongTien = 0;
+                foreach (var ct in chiTietList)
+                {
+                    table.AddCell(ct.TenQuanAo);
+                    table.AddCell($"{ct.DonGiaBan:N0} VNĐ");
+                    table.AddCell(ct.SoLuong.ToString());
+                    table.AddCell($"{ct.GiamGia:N0} VNĐ");
+                    decimal thanhTien = ct.ThanhTien - ct.GiamGia;
+                    table.AddCell($"{thanhTien:N0} VNĐ");
+                    tongTien += thanhTien;
+                }
+
+                doc.Add(table);
+                doc.Add(new Paragraph(" "));
+                doc.Add(new Paragraph($"Tong cong: {tongTien:N0} VND", FontFactory.GetFont(BaseFont.TIMES_BOLD, 12)));
+                doc.Close();
+            }
+
+            MessageBox.Show($"Xuất hóa đơn thành công tại:\n{filePath}", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
     }
 }
+    
+
